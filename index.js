6b2f1b5579f1a39da4ed238388a8d8d596a4367a
DJ-Web-design <djgm1206@gmail.com>
2018-10-12 03:44:52 -0430

    Add 404 Page and data ops routes

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const public = join(__dirname, "public");
 
 app.use(express.static(public));
 app
+	.get("*",(req, res)=>{
+		res.status(404).send("not-found");
+	})
 	.get("/",(req, res)=>{
 		res.writeHeader(200, {"Content-Type":"text/html; charset=utf-8"})
 		res.sendFile("index.html");
@@ -21,6 +24,12 @@ app
 	.get("*.css",(req, res)=>{
 		res.writeHeader(200, {"Content-Type":"text/css"});
 		res.sendFile(req.url);
+	})
+	.get("/change-data", (req, res)=>{
+
+	})
+	.get("/fetch-data", (req, res)=>{
+
 	})
 	.listen(PORT, ()=>{
 		console.log(`Listening on port ${PORT}`);
